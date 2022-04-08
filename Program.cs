1cb7a703f0ac529c84324268a21a9b11a3ff1d02
satyamwathrey <swathrey1@gmail.com>
2022-04-08 22:11:11 +0530

    Ability to add multiple persons in AddressBook

--- Program.cs
+++ Program.cs
@@ -8,12 +8,38 @@ namespace AddressBook
 {
     class AddressBookMain
     {
-        public static List<Contacts> listCon = new List<Contacts>();
+        
         static void Main()
         {
-
             Console.WriteLine("Welcome to Address Book Program");
-            Calling.CallingAddressBook();
+            Console.WriteLine("Enter 1 to Add Contact\nEnter 2 to Edit Contact\nEnter 3 to Delete Person From Contact\nEnter 4 to Exit");
+            int key = Convert.ToInt32(Console.ReadLine());
+            switch (key)
+            {
+                case 1:
+                    Console.WriteLine("Enter how many contacts you want to add?");
+                    int howMany = int.Parse(Console.ReadLine());
+                    for (int i = 1; i <= howMany; i++)
+                    {
+                        Contacts.AddContacts();
+                    }
+                    AddressBookMain.Main();
+                    break;
+                case 2:
+                    Contacts.EditContact();
+                    AddressBookMain.Main();
+                    break;
+                case 3:
+                    Contacts.DeleteContact();
+                    AddressBookMain.Main();
+                    break;
+                case 4:
+                    break;
+                default:
+                    Console.WriteLine("Try Again. Wrong key");
+                    AddressBookMain.Main();
+                    break;
+            }
         }
   
     }
