286bc3e4e6bd5f17e92735ff52da7f9db6e336ce
satyamwathrey <swathrey1@gmail.com>
2022-05-07 23:29:37 +0530

    refactor to add multiple Address Book to the system. Each Address Book has a unique name

---
+++ AddressBookSystem.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    class AddressBookSystem
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=-=-=-=-=-=-=-=Welcome to address book system=-=-=-=-=-=-=-=");
+
+            AddressBook addressBook = new AddressBook();
+
+            Console.WriteLine("Please choose an option:");
+            Console.WriteLine("1. View Contact \n2. Add New Contact(s) \n3. Edit Contact \n4: Delete Contact " +
+                "\n5: Add Multiple Addressbook\n6: Exit\n");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    addressBook.ViewContact();
+                    break;
+                case 2:
+                    addressBook.AddNewContact();
+                    addressBook.ViewContact();
+                    break;
+                case 3:
+                    Console.WriteLine("\nEnter First name to edit it's contact details");
+                    string input = Console.ReadLine();
+                    addressBook.EditContact(input);
+                    addressBook.ViewContact();
+                    break;
+                case 4:
+                    Console.WriteLine("\nEnter First name to delete it's contact details");
+                    string fName = Console.ReadLine();
+                    Console.WriteLine("Enter last name to delete it's contact details");
+                    string lName = Console.ReadLine();
+                    addressBook.DeleteContact(fName, lName);
+                    addressBook.ViewContact();
+                    break;
+                case 5:
+                    addressBook.AddNewAddressBook();
+                    addressBook.ViewContact();
+                    break;
+                case 6:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Invalid Input!");
+                    break;
+            }
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ IContact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    interface IContact
+    {
+        List<Contact> AddNewContact();
+        void ViewContact();
+        void EditContact(string input);
+        void DeleteContact(string fName, string lName);
+        void AddNewAddressBook();
+    }
+}
