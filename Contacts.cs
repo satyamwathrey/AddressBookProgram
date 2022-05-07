cb6e525aeb424c02c00fd974706300c9412c11f1
satyamwathrey <swathrey1@gmail.com>
2022-05-07 20:48:19 +0530

    Ability to ensure there is no Duplicate Entry of the same person in the AddressBook

---
+++ Contacts.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProgram
+{
+    class Contact
+    {
+
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string city;
+        public string state;
+        public int zipcode;
+        public long phoneNumber;
+        public string email;
+
+        public Contact(string firstName, string lastName, string address, string city, string state, int zipcode, long phoneNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zipcode = zipcode;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+        public override string ToString()
+        {
+            return $"firstname:{firstName} lastname:{lastName} address:{address} city:{city} state:{state} zipcode:{zipcode} phone:{phoneNumber} email:{email}";
+        }
+    }
+}
